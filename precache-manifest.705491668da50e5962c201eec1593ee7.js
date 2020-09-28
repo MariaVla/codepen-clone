ab604d808b7cc2343be4b422248e8cec9b2667e9
Majo Vlasanovich <majo@vlasanovich.com>
2020-09-28 18:06:48 -0300

    Updates

---
+++ precache-manifest.705491668da50e5962c201eec1593ee7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ff1980d3c3365cee13a1f693355b4b",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "5348779e828f0f106dae",
+    "url": "/codepen-clone/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "b6bf61df8c0f825471a0",
+    "url": "/codepen-clone/static/css/main.25c9076c.chunk.css"
+  },
+  {
+    "revision": "5348779e828f0f106dae",
+    "url": "/codepen-clone/static/js/2.07b21764.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.07b21764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6bf61df8c0f825471a0",
+    "url": "/codepen-clone/static/js/main.74375402.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
